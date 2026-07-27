6ee9b775e287bc04510286c94ce83489652fc261
SUNO <tnsgusdh1234@gmail.com>
2026-07-27 01:09:40 -0700

    chore: 노출된 API 키 포함 테스트 스크립트 제거 및 gitignore 추가

--- test_weather.sh
+++
@@ -1,9 +0,0 @@
-curl -G "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst" \
-  --data-urlencode "serviceKey=b822316089526ad4f95916723eccd60b338cb51473270ee33cd55967d7d8575f" \
-  --data-urlencode "numOfRows=100" \
-  --data-urlencode "pageNo=1" \
-  --data-urlencode "dataType=JSON" \
-  --data-urlencode "base_date=20260722" \
-  --data-urlencode "base_time=0500" \
-  --data-urlencode "nx=90" \
-  --data-urlencode "ny=100" 
